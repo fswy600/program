10ac82764e83015d8f245a9c8e6d3ed86bd69b12
fswy600 <46289403+fswy600@users.noreply.github.com>
2024-10-08 15:56:35 +0800

    Create select_sort.c

---
+++ selectsort/select_sort.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+void select_sort(int arr[],int size)
+{
+	int i=0,j=0;
+	int k=0;
+	for(i=0;i<size;i++){
+		k=i;
+		for(j=i+1;j<size;j++){
+			if(arr[k]<arr[j]){
+				k=j;
+			}
+
+		}
+		if(k!=i){
+			int tmp=arr[k];
+			arr[k]=arr[i];
+			arr[i]=tmp;
+		}
+	}
+}
+int main()
+{
+	// int arr[]={3,6,1,9,4,2,0,5,8,7};
+	// int len=sizeof(arr)/sizeof(arr[0]);
+	// select_sort(arr,len);
+	// int i=0;
+	// for(i=0;i<len;i++){
+	// 	printf("%d ",arr[i]);
+	// }
+	// printf("\n");
+	int size=30000;
+	int array[size];
+	for (int i = 0; i < size; i++) {  
+        array[i] = size-i;
+    }
+	select_sort(array,size);
+	return 0;
+}
